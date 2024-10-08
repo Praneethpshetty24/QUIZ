a5b0771f5688e0ed5376c76f9a5656a029f0141c
Praneethpshetty24 <praneeth666s@gmail.com>
2024-10-08 12:29:40 +0530

    changed config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,19 +1,20 @@
-// Firebase.js
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage'; // Import Firebase Storage
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjMKNeMDucWjlKuCaqhJtA_t18lvElY4Y",
-  authDomain: "quiz-e4cb6.firebaseapp.com",
-  projectId: "quiz-e4cb6",
-  storageBucket: "quiz-e4cb6.appspot.com",
-  messagingSenderId: "192333827310",
-  appId: "1:192333827310:web:3316ad5f3cc0d13191b7ff",
-  measurementId: "G-F0QN18N2JM"
+  apiKey: "AIzaSyAEeojPeB5pLBBTpOhqcnBvKC_rA3kWz1A",
+  authDomain: "quiz-tech-5193c.firebaseapp.com",
+  projectId: "quiz-tech-5193c",
+  storageBucket: "quiz-tech-5193c.appspot.com",
+  messagingSenderId: "363035779376",
+  appId: "1:363035779376:web:37df73d6aafa7acb7de8f5",
+  measurementId: "G-3E3GL6H05E"
 };
 
 // Initialize Firebase
@@ -23,4 +24,5 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app); // Initialize Firebase Storage
 
-export { db, auth, storage }; // Export Firestore, Auth, and Storage
+// Export Firestore, Auth, and Storage
+export { db, auth, storage };
